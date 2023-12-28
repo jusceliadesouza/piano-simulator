bb106822fa6c74c030e6afe72c5fbb9c892ab268
Juscélia de Souza <jusceliadesousa@gmail.com>
2023-12-28 14:38:23 -0300

    Update volume slider and fix code formatting

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -1,26 +1,34 @@
-const pianoKeys = document.querySelectorAll('.piano-keys .key');
+const pianoKeys = document.querySelectorAll(".piano-keys .key");
+
+const volumeSlider = document.querySelector(".volume-slider input");
 
 let mapedKeys = [];
-let audio = new Audio('./src/tunes/a.wav');
+let audio = new Audio("./src/tunes/a.wav");
 
 const playTune = (key) => {
-  audio.src = `src/tunes/${key}.wav`
-  audio.play()
+  audio.src = `src/tunes/${key}.wav`;
+  audio.play();
 
   const clickedKey = document.querySelector(`[data-key="${key}"]`);
-  clickedKey.classList.add('active');
+  clickedKey.classList.add("active");
   setTimeout(() => {
-    clickedKey.classList.remove('active');
-  }, 150)
-}
+    clickedKey.classList.remove("active");
+  }, 150);
+};
 
 pianoKeys.forEach((key) => {
-  key.addEventListener('click', () => playTune(key.dataset.key));
+  key.addEventListener("click", () => playTune(key.dataset.key));
   mapedKeys.push(key.dataset.key);
 });
 
-document.addEventListener('keydown', (e) => {
-  if(mapedKeys.includes(e.key)) {
+document.addEventListener("keydown", (e) => {
+  if (mapedKeys.includes(e.key)) {
     playTune(e.key);
   }
 });
+
+const handleVolumeChange = (e) => {
+  audio.volume = e.target.value;
+};
+
+volumeSlider.addEventListener("input", handleVolumeChange);
